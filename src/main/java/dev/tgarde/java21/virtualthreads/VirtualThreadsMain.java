5af95e5a53a571546fe40565026806343ef25541
Tejas Garde <tgarde@cisco.com>
2024-02-29 17:15:10 +0530

    first Virtual Thread method

--- src/main/java/dev/tgarde/java21/virtualthreads/VirtualThreadsMain.java
+++ src/main/java/dev/tgarde/java21/virtualthreads/VirtualThreadsMain.java
@@ -2,40 +2,58 @@ package dev.tgarde.java21.virtualthreads;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.RunnableFuture;
 
 public class VirtualThreadsMain {
 
   public static void main(String[] args) {
-    PlatformThreads.platformThreadRun();
+   PlatformThreads.platformThreadRun();
+   VirtualThread.fromVirtualThread();
   }
 
+}
+
+
+class VirtualThread {
+
+  public static void fromVirtualThread(){
+    Thread thread = Thread.ofVirtual().start(RunnableInstance.getRunnable());
+    try {
+      thread.join();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+  }
 }
 
 
 class PlatformThreads {
 
   public static void platformThreadRun() {
-    ExecutorService service = Executors.newFixedThreadPool(130);
+    ExecutorService service = Executors.newFixedThreadPool(10);
 
-    for (int i = 0; i < 1000; i++) {
-      service.submit(() -> {
-        System.out.println("Running Thread " + Thread.currentThread().getName());
-        try {
-          Thread.sleep(100);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+    Runnable runnable = RunnableInstance.getRunnable();
 
-      });
+    for (int i = 0; i < 100; i++) {
+      service.submit(runnable);
     }
 
-    try {
-      Thread.sleep(500005);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
     service.shutdown();
 
 
+  }
+
+}
+
+class RunnableInstance {
+  public static Runnable getRunnable(){
+    return () -> {
+      System.out.println("Running Thread " + Thread.currentThread());
+      try {
+        Thread.sleep(100);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    };
   }
 }
