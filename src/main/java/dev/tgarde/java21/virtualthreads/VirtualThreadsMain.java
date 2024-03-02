21637a458c4169a11f74540b5da8280bf48bc4a4
Tejas Garde <tgarde@cisco.com>
2024-03-02 22:41:31 +0530

    Virtual ThreadTaskExecutor

--- src/main/java/dev/tgarde/java21/virtualthreads/VirtualThreadsMain.java
+++ src/main/java/dev/tgarde/java21/virtualthreads/VirtualThreadsMain.java
@@ -1,14 +1,20 @@
 package dev.tgarde.java21.virtualthreads;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.RunnableFuture;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class VirtualThreadsMain {
 
   public static void main(String[] args) {
-   PlatformThreads.platformThreadRun();
-   VirtualThread.fromVirtualThread();
+    PlatformThreads.platformThreadRun();
+    VirtualThread.fromVirtualThread();
+    VirtualThread.fromVirtualThreadBuilder();
+    VirtualThread.fromExecutorService();
   }
 
 }
@@ -16,6 +22,16 @@ public class VirtualThreadsMain {
 
 class VirtualThread {
 
+  public static void fromExecutorService(){
+    try (ExecutorService myExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
+      IntStream.range(0,99)
+              .forEach(val -> RunnableInstance.executeFuture(myExecutor.submit(RunnableInstance.getRunnable())));
+    }
+  }
+
+  /**
+   * Creating Thread from Thread.ofVirtual method
+   */
   public static void fromVirtualThread(){
     Thread thread = Thread.ofVirtual().start(RunnableInstance.getRunnable());
     try {
@@ -24,22 +40,28 @@ class VirtualThread {
       e.printStackTrace();
     }
   }
+
+  public static void fromVirtualThreadBuilder(){
+    Thread.Builder builder = Thread.ofVirtual().name("MyThread");
+    Thread thread = builder.start(RunnableInstance.getRunnable());
+    try {
+      thread.join();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+
+  }
 }
 
 
 class PlatformThreads {
 
   public static void platformThreadRun() {
-    ExecutorService service = Executors.newFixedThreadPool(10);
-
-    Runnable runnable = RunnableInstance.getRunnable();
-
-    for (int i = 0; i < 100; i++) {
-      service.submit(runnable);
+    try (ExecutorService service = Executors.newFixedThreadPool(10)) {
+      Runnable runnable = RunnableInstance.getRunnable();
+      IntStream.range(0,99)
+              .forEach(val ->  RunnableInstance.executeFuture(service.submit(runnable)));
     }
-
-    service.shutdown();
-
 
   }
 
@@ -55,5 +77,14 @@ class RunnableInstance {
         e.printStackTrace();
       }
     };
+  }
+
+
+  public static void executeFuture(Future<?> future){
+    try {
+      future.get();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
   }
 }
